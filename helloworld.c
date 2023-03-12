570658c5cda442543ca468bc27c97d6712108100
camila <camilabia.csilva@gmail.com>
2023-03-11 23:22:57 -0300

    mudando saida novamente

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void){
 	char var_name[20];
-	printf("Diga seu nome agora? ");
+	printf("Diga seu nome agora agora? ");
 	scanf("%s", var_name);
 	printf("Bem vindo %s!\n", var_name);
 	return 0;
